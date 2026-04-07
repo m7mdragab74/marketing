6d738e5ac8f35919729ec3d98b6af89fa49eb8b0
m7mdragab <mr341349@gmail.com>
2026-04-08 01:38:39 +0200

    create metrics

--- marketing_project.py
+++ marketing_project.py
@@ -13,3 +13,22 @@ df.isnull().sum()
 df['Region'] = df['Region'].fillna('Unknown')
 df['Impressions'] = df['Impressions'].fillna(df['Impressions'].mean())
 print(df.isnull().sum())
+
+#metrics
+# CTR
+df['CTR'] = df['Clicks'] / df['Impressions']
+
+# Conversion Rate
+df['Conversion_Rate'] = df['Conversions'] / df['Clicks']
+
+# CPA (Cost per Acquisition)
+df['CPA'] = df['Spend'] / df['Conversions']
+
+# ROAS (Return on Ad Spend)
+df['ROAS'] = df['Revenue'] / df['Spend']
+
+#Profit / Profit Margin
+df['Profit'] = df['Revenue'] - df['Spend']
+df['Profit_Margin'] = df['Profit'] / df['Revenue']
+
+print(df.head())
